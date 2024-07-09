6eba9dc97e6a332a844d26b13218a299413168ad
root <root@eeee.>
2024-07-09 11:35:53 +0900

    롤백

--- home/urls.py
+++ home/urls.py
@@ -2,10 +2,10 @@ from django.urls import path
 from django.shortcuts import redirect
 from . import views
 
-def redirect_to_login(request):
-    return redirect('login')
+#def redirect_to_login(request):
+#    return redirect('login')
 
 urlpatterns = [
-    path('index', redirect_to_login, name='index'),
+    path('index', views.index, name='index'),
     path('', views.login_view, name='login'),
 ]

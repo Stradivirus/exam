286aa4bbece4692aa57bfdfe64f7fb71170d2d85
root <root@eeee.>
2024-07-09 11:29:34 +0900

    로그인 수정

--- home/urls.py
+++ home/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
+from django.shortcuts import redirect
 from . import views
 
+def redirect_to_login(request):
+    return redirect('login')
+
 urlpatterns = [
-    path('index', views.index, name='index'),
+    path('index', redirect_to_login, name='index'),
     path('', views.login_view, name='login'),
 ]
-
-
